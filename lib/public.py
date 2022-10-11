dec6bd4955f8c35fdb94a21e3f8ee83ccbb71c04
iuime <41294179+iuime@users.noreply.github.com>
2022-10-11 18:16:09 +0800

    Update public.py

--- lib/public.py
+++ lib/public.py
@@ -1,20 +1,47 @@
 # -*- coding:utf8 -*- #
-import smtplib,email,email.MIMEMultipart,email.MIMEBase,email.MIMEText
-import email.MIMEImage,base64,sys,re
+import smtplib
+import requests
+import base64,sys,re,json
 
-def sendMail(mailmsg,tomail,title):
-    mail_recv=re.split('[,;]',tomail)
-    femail=('test@hotmail.com')
-    temail =tomail
-    msg=email.MIMEMultipart.MIMEMultipart()
-    msg['From'] = femail
-    msg['To'] = ';'.join(mail_recv)
-    msg['Subject'] = title
-    msg['Reply-To'] = femail
-    body=email.MIMEText.MIMEText(mailmsg,_charset='utf8')
-    msg.attach(body)
-    smtp = smtplib.SMTP('10.11.11.11','25')
-    smtp.login('user', '123456') 
-    smtp.sendmail(femail,mail_recv,msg.as_string())
-    smtp.close()
-    print 'All mail were sended!'
+
+def sendWebhook(alertMsg,Webhookkey,title):
+    #钉钉告警配置
+    #url = "https://oapi.dingtalk.com/robot/send?access_token="+Webhookkey
+    #headers = {'Content-Type': 'application/json;charset=utf-8'}
+    #data_text = {
+    #"msgtype": "text",
+    #"text": {
+    #    "content": "端口扫描机器人\n"+alertMsg
+    #},
+    #    "isAtAll": False  #此处为是否@所有人
+    #}
+    
+    #发送链接消息
+    #data_link = {
+    #"msgtype": "link",
+    #"link": {
+    #    "text": "任务通知-这是一条消息", #消息内容
+    #    "title": "历史的车轮滚滚向前", #消息标题
+    #    "picUrl": "https://i.loli.net/2021/09/26/JNTYrWKywd34Lqb.jpg",#图片链接
+    #"messageUrl": "https://developers.dingtalk.com/document/robots/custom-robot-access/title-zob-eyu-qse" 
+    #消息跳转链接
+
+#}}
+
+    #Lark 告警配置
+    url = "https://open.larksuite.com/open-apis/bot/v2/hook/"+Webhookkey
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    data_text = {
+    "msg_type": "text",
+    "content": {
+        "text": "端口扫描机器人\n"+alertMsg
+    }
+    }
+    print (alertMsg)
+
+
+    # 发送文字消息
+    r = requests.post(url,data=json.dumps(data_text),headers=headers)
+    # 发送链接消息
+    #r = requests.post(url, data=json.dumps(data_link), headers=headers)
+    print(r.text)
